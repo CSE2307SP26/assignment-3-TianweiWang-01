fa593ce591406d109dd068de46e06937ee32542e
Tianwei Wang <tianwei@wustl.edu>
2026-02-12 11:24:37 -0600

    Add files via upload
    2

--- grading.sh
+++ grading.sh
@@ -3,17 +3,17 @@ EXPECTED_OUTPUT="$1"
 STUDENT_OUTPUT="$2"
 
 while read -r WUSTL_KEY; do
-    git clone "https://github.com/CSE237SP25/$WUSTL_KEY.git "
+    git clone "https://github.com/CSE237SP25/$WUSTL_KEY.git"
     cd "$WUSTL_KEY" 
     git checkout cipher
     git checkout "$(git log -1 --before="2026-02-12 10:00:00 -0600" --format="%H" HEAD)"
 
     javac Cipher.java
-    java Cipher > "$STUDENT_OUT"
+    java Cipher > "$STUDENT_OUTPUT"
 
     SCORE=0
  
-    if [ -f "$STUDENT_OUT" ]; then
+    if [ -f "$STUDENT_OUTPUT" ]; then
         
         if  diff -q "$STUDENT_OUTPUT" "../$EXPECTED_OUTPUT" >/dev/null  ; then
             SCORE=1
